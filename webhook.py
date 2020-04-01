d6c40b1c71a738f077949c3ae8015eee91152f70
Phani Kumar <58216617+funnyPhani@users.noreply.github.com>
2020-04-01 16:39:45 +0530

    Add files via upload

---
+++ webhook.py
@@ -0,0 +1,53 @@
+import json #to convert list and dictionary to json
+import os
+import requests
+from flask import Flask #it is microframework to develop a web app
+from flask import request
+from flask import make_response
+#Falsk app for our web app
+
+app=Flask( name )
+
+# app route decorator. when webhook is called, the decorator would call the functions which are e defined
+
+@app.route('/webhook', methods=['POST'])
+
+def webhook():
+	req=request.get_json(silent=True, force=True) 
+	print(json.dumps(req, indent=4))
+	res=makeResponse(req) 
+	res=json.dumps(res, indent=4) r=make_response(res)
+	r.headers['Content-Type']='application/json'
+	return r
+	
+	
+	
+def makeResponse(req): 
+	result=req.get("queryResult")
+	parameters=result.get("parameters") 
+	city=parameters.get("geo-city") 
+	date=parameters.get("date")
+	
+r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q=hyderabad,in&a ppid=db91df44baf43361cbf73026ce5156cb')
+	json_object=r.json() 
+	weather=json_object['list']
+	condition=weather[0]['weather'][0]['description']
+	speech="The forecast for "+city+ " for "+date+" is "+ condition
+	return{ 
+	"fulfillmentMessages": [
+	{ 
+	"text": {
+	  "text": speech
+	 } 
+	}]}
+	
+if __name__=='__main__': 
+	port=int(os.getenv('PORT',5000)) 
+	print("starting on port %d" % port) 
+	app.run(debug=False, port=port, host='0.0.0.0')
+
+
+
+
+
+
